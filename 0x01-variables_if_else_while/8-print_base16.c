f27b942cd128e39a5484c13b8eb9407203817ac1
Blessed Uade Idon <donbless0527@gmail.com>
2023-05-20 04:32:14 -0700

    C - Variables, if, else, while

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+  *main - print all numbers in base 16
+  *Return: Alawys 0
+  */
+int main(void)
+{
+	int n = 48;
+	int l = 97;
+
+	for (n = 48; n <= 57; n++)
+	{
+		putchar(n);
+	}
+	for (l = 97; l <= 102; l++)
+	{
+		putchar(l);
+	}
+	putchar('\n');
+	return (0);
+}
