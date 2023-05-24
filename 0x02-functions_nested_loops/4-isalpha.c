95ac4a582cd065bdcd784d87e0cb4eb174751f96
Blessed Uade Idon <donbless0527@gmail.com>
2023-05-24 06:22:07 -0700

    C - functions nested loops

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -6,15 +6,9 @@
  * Return: (1) if c is a letter, (0) otherwise
  */
 int _isalpha(int c)
-
 {
-	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
-{
-	return (1);
-}
-else
-{
-	return (0);
-}
-        putchar('\n');
+	if ((c >= 97 && c <= 122) || (c >= 65 && C <= 90))
+		return (1);
+	else
+		return (0);
 }
