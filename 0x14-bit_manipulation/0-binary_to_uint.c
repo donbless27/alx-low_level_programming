e73aac49f6bd828991d187cebef89c1afa08a2f3
donbless27 <donbless0527@gmail.com>
2023-07-07 08:20:48 -0700

    Bit manipulation in C

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -26,28 +26,3 @@ unsigned int binary_to_uint(const char *b)
 	}
 	return (result);
 }
-
-#include <stdio.h>
-
-/**
-  * main - the main function that generaes the codes
-  * @n: numbers to be printed out
-  * Return: 0
-  */
-int main(void)
-{
-	unsigned int n;
-
-	n = binary_to_uint("1");
-	printf("%u\n", n);
-	n = binary_to_uint("101");
-	printf("%u\n", n);
-	n = binary_to_uint("1e01");
-	printf("%u\n", n);
-	n = binary_to_uint("1100010");
-	printf("%u\n", n);
-	n = binary_to_uint("0000000000000000000110010010");
-	printf("%u\n", n);
-	return (0);
-}
-
