a2222bb9179929ef5e877b1f2282287ae1658b04
donbless27 <donbless0527@gmail.com>
2023-07-07 09:11:58 -0700

    Bit manipulation in C

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,28 +1,56 @@
 #include "main.h"
-#include <string.h>
 
 /**
-  * binary_to_uint - function that converts a binary
-  * number to an unsigned int.
-  * @b: binary pointer to a string
-  * Return: unsigned int
-  */
+ * binary_to_uint- convert binary to unsigned int
+ * @b: binary
+ * Return: unsigned int
+ */
 unsigned int binary_to_uint(const char *b)
 {
-	if (b == NULL)
-	{
-		return (0);
-	}
-	size_t length = strlen(b);
-	unsigned int result = 0;
 
-	for (size_t i = 0; i < length; i++)
+	int string = 0, j;
+	unsigned int result = 0, non_binary = 0;
+
+	if (b == NULL)
+		return (non_binary);
+
+	while (b[string] != '\0')
+		string++;
+	string -= 1;
+	j = 0;
+	while (b[j])
 	{
-		if (b[i] != '0' && b[i] != '1')
-		{
-			return (0);  /** Invalid character found, return 0 */
-		}
-		result = (result << 1) + (b[i] - '0');
+		if ((b[j] != '0') && (b[j] != '1'))/*&& logical AND operator*/
+			return (non_binary);
+
+		if (b[j] == '1')
+			result += (1 * (1 << string));/*<< bitwise left shift operator*/
+		j++;
+
+		string--;
 	}
 	return (result);
+}
+
+#include <stdio.h>
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	unsigned int n;
+
+	n = binary_to_uint("1");
+	printf("%u\n", n);
+	n = binary_to_uint("101");
+	printf("%u\n", n);
+	n = binary_to_uint("1e01");
+	printf("%u\n", n);
+	n = binary_to_uint("1100010");
+	printf("%u\n", n);
+	n = binary_to_uint("0000000000000000000110010010");
+	printf("%u\n", n);
+	return (0);
 }
